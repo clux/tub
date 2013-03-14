111e72782eff4f42ccd5d9a0a2113283da79eb44
clux <analsandblaster@gmail.com>
2013-03-14 14:38:09 +0000

    damnit. forgot newline

--- tub.js
+++ tub.js
@@ -117,7 +117,7 @@ Tapper.prototype._transform = function (line, encoding, cb) {
     }
     else if (type === 'bail') {
       this.bail = m[1]; // bailout reason (may be empty string)
-      this.push('✗ Bail out! ' + m[1]);
+      this.push('✗ Bail out! ' + m[1] + '\n');
       this.end();
     }
     // ignore type === 'comment' in this module
